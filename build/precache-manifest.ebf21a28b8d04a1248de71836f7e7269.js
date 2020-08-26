247d460f99b9300c5d2216d7a52c8f3271a6dc44
unknown <luka.patrun@gmail.com>
2020-08-26 15:17:17 +0200

    to gh pages

---
+++ build/precache-manifest.ebf21a28b8d04a1248de71836f7e7269.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77b0ce7b01d2ae7e527aa456c7a8edd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be126dadc267b87e4844",
+    "url": "/portfolio/static/css/main.adfd698d.chunk.css"
+  },
+  {
+    "revision": "2335c57a74297b0b08e5",
+    "url": "/portfolio/static/js/2.79e9ed1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.79e9ed1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be126dadc267b87e4844",
+    "url": "/portfolio/static/js/main.dac40b52.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "27554e2c9576989d98e56e3f648b604a",
+    "url": "/portfolio/static/media/angular.27554e2c.png"
+  },
+  {
+    "revision": "3f63726fa02c3ceb1d01061ad2a4feab",
+    "url": "/portfolio/static/media/filmotekaMock.3f63726f.jpg"
+  },
+  {
+    "revision": "8df97988fd9597fdfcaecead59345e36",
+    "url": "/portfolio/static/media/governorMock.8df97988.jpg"
+  },
+  {
+    "revision": "18743db9be6925fc61618f17b0c95b8b",
+    "url": "/portfolio/static/media/headbckgr.18743db9.jpg"
+  },
+  {
+    "revision": "e9a7d1bb00d12a5d36437b0e4673420b",
+    "url": "/portfolio/static/media/herosMock.e9a7d1bb.jpg"
+  },
+  {
+    "revision": "c429502e02ffa893b46bb4916f95ecb3",
+    "url": "/portfolio/static/media/kenazMock.c429502e.jpg"
+  },
+  {
+    "revision": "7a912e784307c38ff0d7615470632462",
+    "url": "/portfolio/static/media/portfolioMock.7a912e78.jpg"
+  },
+  {
+    "revision": "22fd418bdb992e2a62bbe58fef7cbde0",
+    "url": "/portfolio/static/media/portfoliobcgr.22fd418b.jpg"
+  },
+  {
+    "revision": "93a2498ba005526d9681bb0c3d524309",
+    "url": "/portfolio/static/media/profilepic.93a2498b.jpg"
+  },
+  {
+    "revision": "4603f0d1d6482913bccc9693c93bcf5a",
+    "url": "/portfolio/static/media/react.4603f0d1.png"
+  },
+  {
+    "revision": "3d2028ac9cf5a8620405b6b721b89cbd",
+    "url": "/portfolio/static/media/vue.3d2028ac.png"
+  }
+]);
